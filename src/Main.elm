d46c09d3c8d2822e4d93a65b164945fd3bff371f
Aaron Lebahn <cplusplus328@gmail.com>
2020-03-08 21:06:50 -0500

    Set up canvas and draw menu

--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,16 @@
-module Main exposing (..)
+module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html)
+import Html.Attributes exposing (class)
+import Canvas exposing (shapes, rect, text)
+import Canvas.Settings exposing (Setting, fill)
+import Canvas.Settings.Text exposing (font)
+import Color
 
 -- MAIN
 
+main : Program () Model Msg
 main =
   Browser.element
   { init = init
@@ -16,12 +21,13 @@ subscriptions : Model -> Sub Msg
 subscriptions model = Sub.none
 
 -- MODEL
-type alias Model = Int
+type GameState = Menu
+type alias Model = { state: GameState }
 
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  (0, Cmd.none)
+  (Model Menu, Cmd.none)
 
 
 
@@ -33,22 +39,36 @@ type Msg
   | Decrement
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-  case msg of
-    Increment ->
-      (model + 1, Cmd.none)
-
-    Decrement ->
-      (model - 1, Cmd.none)
-
-
+  (model, Cmd.none)
 
 -- VIEW
-
+width : Int
+width = 95
+height: Int
+height = 60
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+  Canvas.toHtml
+      ( width, height )
+      [ class "gameCanvas"]
+      (background model)
+
+gameFont : Setting
+gameFont = font {size = 8, family = "'Press Start 2P'"}
+
+background : Model -> List Canvas.Renderable
+background model =
+  case model.state of
+     Menu -> [ text [ gameFont ] (24, 12) "MORTLE"
+             , text [ gameFont ] (4, 24) "New Game"
+             , text [ gameFont ] (4, 32) "Continue" ]
+
+  --setFont context "8px 'Press Start 2P'"
+  --fillText context "MORTLE" 24.0 12.0
+  --fillText context "New Game" 4.0 24.0
+  --fillText context "Continue" 4.0 32.0
+
+clearScreen : Canvas.Renderable
+clearScreen =
+    shapes [ fill Color.white ] [ rect ( 0, 0 ) (toFloat width) (toFloat height) ]
