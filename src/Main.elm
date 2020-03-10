fef0e919e89416b6b20fe31cc8d70fc40180ed15
Aaron Lebahn <cplusplus328@gmail.com>
2020-03-09 21:22:46 -0500

    Implement advancing to aim and drawing level

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,6 @@
 module Main exposing (main)
 
+import Array exposing (Array)
 import Browser
 import Browser.Events exposing (onKeyDown)
 import Html exposing (Html)
@@ -9,6 +10,156 @@ import Canvas.Settings exposing (Setting, fill)
 import Canvas.Settings.Text exposing (font)
 import Color
 import Json.Decode as Decode
+
+o : Bool
+o = False
+i : Bool
+i = True
+
+levels : Array (Array (Array Bool))
+levels = Array.map (\listList -> Array.map Array.fromList (Array.fromList listList)) (Array.fromList
+  [
+    [
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o,o],
+      [o,o,o,i,i,i,i,i,i,i,i,i,i,i,i,i,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,i,o,o,o,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,i,o,o,o,o,o,o,o,o,o]
+    ],
+    [
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,i,i,i,i,i,o,i,i,i,i,i,i,i,o,i,i],
+      [o,o,i,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o],
+      [o,i,i,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o]
+    ],
+    [
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,i,i,i,i,i,o,o,o],
+      [o,i,i,o,o,o,o,o,o,o,o,o,i,o,i,o,o,o,o],
+      [o,o,i,i,o,o,o,o,o,o,o,o,i,o,i,o,o,o,o],
+      [i,o,i,o,i,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,i,o,o,i,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,i,i,o,o,o,i,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,i,o,o,o,o,i,o,o,o,o,o,o,o,o,o,o,o],
+      [i,o,i,o,o,o,o,o,i,o,o,o,o,o,o,o,o,o,o],
+      [o,o,i,o,o,o,o,o,o,i,o,o,o,o,o,o,o,o,o],
+      [o,i,i,o,o,o,o,o,o,o,i,o,o,o,o,o,o,o,o],
+      [o,o,i,o,o,o,o,o,o,o,o,i,o,o,o,o,o,o,o]
+    ],
+    [
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,i,i,o,i,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,i,i,o,o,o,i,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o,o,o,o],
+      [o,o,o,o,o,i,i,o,o,o,o,o,i,o,o,o,o,o,o],
+      [o,o,o,o,o,o,i,o,i,o,i,o,i,o,o,o,o,o,o],
+      [o,o,o,i,i,o,i,o,i,o,o,o,i,o,o,o,o,o,o],
+      [o,o,o,o,o,o,i,i,i,o,i,o,i,o,o,o,o,o,o],
+      [o,i,i,o,o,o,i,o,i,o,i,o,i,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o,o,o,o]
+    ],
+    [
+      [o,o,o,o,o,o,o,i,o,o,o,o,o,o,o,o,o,o,o],
+      [i,i,i,o,o,o,o,i,o,o,o,o,i,i,o,i,o,o,o],
+      [o,o,o,o,o,o,o,i,o,o,o,o,i,o,o,i,o,o,o],
+      [o,o,o,i,i,i,o,o,o,o,o,o,i,o,i,i,o,o,o],
+      [o,o,o,o,o,i,o,o,o,o,o,o,i,o,o,i,o,o,o],
+      [o,i,i,o,o,i,i,i,i,i,i,o,i,i,o,i,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,i,o,i,o,o,i,o,o,o],
+      [o,o,o,o,i,i,o,o,o,o,i,o,i,o,i,i,o,o,o],
+      [o,o,o,o,i,o,o,o,o,o,i,o,o,o,o,i,o,o,o],
+      [o,o,o,o,i,o,i,i,i,o,i,i,i,i,o,i,o,o,o],
+      [o,i,o,o,o,o,o,o,o,o,o,o,o,o,o,i,o,i,i],
+      [o,i,o,o,i,i,o,o,o,o,o,o,o,o,o,i,o,o,o]
+    ],
+    [
+      [i,i,i,i,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,i,o,i,i,o,o,o,o,o,o,o,o,o,o,o,o],
+      [i,i,o,i,o,o,o,o,o,i,i,o,o,o,o,o,o,o,o],
+      [o,o,o,i,o,o,o,o,o,i,o,o,o,i,i,o,o,o,o],
+      [o,i,i,i,i,i,i,o,o,i,o,o,o,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,i,o,o,i,o,o,o,o,o,o],
+      [i,i,i,o,o,o,o,o,o,i,o,o,i,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,i,o,i,o,o,i,o,o,o,o,o,o],
+      [o,o,o,i,i,i,i,o,o,i,o,o,o,o,o,i,o,i,i],
+      [i,i,o,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o]
+    ],
+    [
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,i,i,i,i,i,i,i,i,i,i,o],
+      [o,o,o,o,o,o,o,i,i,o,o,o,o,o,o,o,o,o,o],
+      [o,i,i,i,i,i,i,i,i,o,o,i,i,i,i,i,i,i,i],
+      [o,o,o,o,o,o,o,o,i,o,o,i,o,o,o,o,o,o,o],
+      [i,o,o,o,o,o,o,i,i,o,o,i,o,o,o,i,o,o,o],
+      [i,i,i,i,i,i,o,o,i,o,i,i,i,i,o,i,o,o,o],
+      [o,o,o,o,o,i,i,o,i,o,o,i,o,o,o,i,o,o,o],
+      [o,o,o,o,o,i,o,o,i,o,o,i,o,i,i,i,o,o,o],
+      [o,o,i,i,o,i,o,i,i,o,o,i,o,o,o,i,o,o,o],
+      [i,o,i,o,o,o,o,o,i,i,o,i,i,i,o,i,o,o,o],
+      [o,o,i,o,o,o,i,o,i,o,o,o,o,o,o,i,o,o,o]
+    ],
+    [
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+      [o,o,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,o,i],
+      [o,o,o,i,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o],
+      [i,i,o,i,o,i,i,i,i,i,i,i,i,i,o,o,i,o,o],
+      [o,o,o,i,o,i,o,o,o,o,o,o,o,i,i,o,i,i,o],
+      [o,i,o,i,o,i,i,o,i,i,i,i,o,i,o,o,o,i,o],
+      [o,o,o,i,o,i,o,o,i,o,o,o,o,i,i,o,o,i,o],
+      [o,i,i,i,o,i,o,i,i,o,i,o,o,i,o,o,o,o,o],
+      [o,o,o,i,o,i,o,o,i,o,i,i,i,i,i,i,i,i,i],
+      [i,o,o,i,o,i,o,i,i,o,o,o,o,o,o,o,o,o,o],
+      [o,o,o,i,o,o,o,o,i,o,o,o,o,o,o,o,o,o,o]
+    ],
+    [
+      [i,i,o,i,o,i,i,i,i,i,o,i,o,i,i,i,i,i,o],
+      [o,o,o,i,o,o,o,o,o,o,o,i,o,o,o,o,o,o,o],
+      [o,i,i,i,i,i,o,i,o,i,i,i,i,i,o,i,o,i,i],
+      [o,o,o,i,o,o,o,i,o,o,o,i,o,o,o,i,o,o,o],
+      [i,i,o,i,o,i,i,i,i,i,o,i,o,i,i,i,i,i,o],
+      [o,o,o,i,o,o,o,i,o,o,o,i,o,o,o,i,o,o,o],
+      [o,i,i,i,i,i,o,i,o,i,i,i,i,i,o,i,o,i,i],
+      [o,o,o,i,o,o,o,i,o,o,o,i,o,o,o,i,o,o,o],
+      [i,i,o,i,o,i,i,i,i,i,o,i,o,i,i,i,i,i,o],
+      [o,o,o,i,o,o,o,i,o,o,o,i,o,o,o,i,o,o,o],
+      [o,i,i,i,i,i,o,i,o,i,i,i,i,i,o,i,o,i,i],
+      [o,o,o,o,o,o,o,i,o,o,o,o,o,o,o,i,o,o,o]
+    ],
+    [
+      [i,o,o,i,o,o,o,o,o,i,o,o,o,o,o,i,o,o,i],
+      [o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o],
+      [o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o],
+      [i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i],
+      [o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o],
+      [o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o],
+      [i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,o],
+      [o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o],
+      [o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o],
+      [i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i],
+      [o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o],
+      [o,i,o,o,o,o,o,i,o,o,o,o,o,i,o,o,o,o,o]
+    ]
+  ])
 
 -- MAIN
 
@@ -24,6 +175,7 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
   case model.state of
     Menu _ -> onKeyDown (Decode.map keyToMenuMsg (Decode.field "key" Decode.string))
+    Aim _ -> onKeyDown (Decode.map keyToAimMsg (Decode.field "key" Decode.string))
 
 keyToMenuMsg : String -> Msg
 keyToMenuMsg key =
@@ -31,18 +183,26 @@ keyToMenuMsg key =
     MenuUp
   else if key == "ArrowDown" then
     MenuDown
+  else if key == "ArrowRight" || key == "Enter" || key == "Enter" || key == " " then
+    MenuAccept
   else
     KeyOther
 
+keyToAimMsg : String -> Msg
+keyToAimMsg key = KeyOther
 
 -- MODEL
-type MenuSelection = NewGame | Continue
+type alias Model = { state : GameState
+                   , startLevel : Int
+                   }
 type GameState = Menu MenuSelection
-type alias Model = { state : GameState }
+               | Aim AimModel
+type MenuSelection = NewGame | Continue
+type alias AimModel = { level : Int }
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  (Model (Menu NewGame), Cmd.none)
+  (Model (Menu NewGame) 0, Cmd.none)
 
 -- UPDATE
 
@@ -58,7 +218,9 @@ update msg model =
     Menu _ -> case msg of
        MenuUp -> ({ model | state = Menu NewGame }, Cmd.none)
        MenuDown -> ({ model | state = Menu Continue }, Cmd.none)
+       MenuAccept -> ({ model | state = Aim (AimModel model.startLevel) }, Cmd.none)
        _ -> (model, Cmd.none)
+    Aim aimModel -> (model, Cmd.none)
 
 -- VIEW
 width : Int
@@ -80,14 +242,31 @@ background : Model -> List Canvas.Renderable
 background model =
   clearScreen ::
   case model.state of
-     Menu _ -> [ text [ gameFont ] (24, 12) "MORTLE"
-               , text [ gameFont ] (4, 24) "New Game"
-               , text [ gameFont ] (4, 32) "Continue" ]
+    Menu _ -> [ text [ gameFont ] (24, 12) "MORTLE"
+              , text [ gameFont ] (4, 24) "New Game"
+              , text [ gameFont ] (4, 32) "Continue" ]
+    Aim aimModel -> drawLevel aimModel.level
+
+drawLevel : Int -> List Canvas.Renderable
+drawLevel levelNum = case Array.get levelNum levels of
+   Nothing -> []
+   Just level -> Array.foldl (++) [] (Array.indexedMap drawRow level)
+
+drawRow : Int -> Array Bool -> List Canvas.Renderable
+drawRow rowNum row = Array.foldl (++) [] (Array.indexedMap (drawCell rowNum) row)
+
+drawCell : Int -> Int -> Bool -> List Canvas.Renderable
+drawCell rowNum colNum cell =
+  if cell then
+    [shapes [fill Color.black ] [ rect (toFloat colNum * 5, toFloat rowNum * 5) 5 5] ]
+  else
+    []
 
 foreground : Model -> List Canvas.Renderable
 foreground model =
   case model.state of
      Menu menuSelection -> [ text [ gameFont ] (72, getYCoordinateFromMenuSelection menuSelection) "<" ]
+     Aim aimModel -> []
 
 getYCoordinateFromMenuSelection : MenuSelection -> Float
 getYCoordinateFromMenuSelection menuSelection =
